e64644d4b3c46c93835bd7fedaafba4bcff27a00
Shannon Scott <154854946+theshannonscott@users.noreply.github.com>
2024-07-12 05:38:08 -0400

    shortened code: made playerChoice and compChoice const variables and made round function

--- script.js
+++ script.js
@@ -3,122 +3,40 @@ alert("Lets play Rock, Paper, Scissors! (Game is Best of Five) \nType answer exa
 let playerScore = 0;
 let computerScore = 0;
 
-alert (`Player's Score =  ${playerScore}\nComputer's Score = ${computerScore}`)
+alert (`SCORE \nPlayer's Score =  ${playerScore}\nComputer's Score = ${computerScore}`)
 
 
 
 function getComputerChoice(choice){
-  return Math.floor(Math.random() * choice); 
+  let answer= Math.floor(Math.random() * choice); 
+  switch (answer){
+    case 0:
+      answer = "rock"
+        break;
+        case 1:
+            answer = "paper";
+            break;
+            case 2:
+                answer = "scissors";
+                break;
+  }
+  return answer
 };
 
 function getPlayerChoice(){
  let choice = prompt ("Rock, Paper, or Scissors?");
 return choice.toLowerCase()};
 
-let playerChoice = getPlayerChoice();
-let compChoice = getComputerChoice(3);
+const playerChoice = getPlayerChoice();
+const compChoice = getComputerChoice(3);
 
-
-switch (compChoice){
-  case 0:
-    compChoice = "rock"
-      break;
-      case 1:
-          compChoice = "paper";
-          break;
-          case 2:
-              compChoice = "scissors";
-              break;
-}
-
-function compare() {if (playerChoice === compChoice){alert (`It's a tie!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1; playerScore = playerScore + 1;}
+function round()
+  {if (playerChoice === compChoice){alert (`It's a tie!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1; playerScore = playerScore + 1;}
 else if (playerChoice === "rock" && compChoice === "scissors") {alert (`Player wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); playerScore = playerScore + 1;}
 else if(playerChoice === "rock" && compChoice === "paper"){alert(`Computer wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1;}
 else if (playerChoice === "paper" && compChoice === "scissors"){alert(`Computer wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1;} 
 else if(playerChoice === "paper" && compChoice === "rock"){alert(`Player wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); playerScore = playerScore + 1;}
 else if (playerChoice === "scissors" && compChoice === "paper"){alert(`Player wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); playerScore = playerScore + 1;}
-else if(playerChoice === "scissors" && compChoice === "rock"){alert(`Computer wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1;};}
+else if(playerChoice === "scissors" && compChoice === "rock"){alert(`Computer wins!!! \nPlayer's choice = ${playerChoice} \nComputer's choice = ${compChoice}`); computerScore = computerScore + 1;} alert(`Next Round`);}
 
-compare(playerChoice, compChoice);
-
-alert("Next Round");
-//second round
-playerChoice = getPlayerChoice();
-compChoice = getComputerChoice(3);
-switch (compChoice){
-  case 0:
-    compChoice = "rock"
-      break;
-      case 1:
-          compChoice = "paper";
-          break;
-          case 2:
-              compChoice = "scissors";
-              break;
-}
-
-compare(playerChoice, compChoice);
-
-alert("Next Round");
-//third round
-playerChoice = getPlayerChoice();
-compChoice = getComputerChoice(3);
-switch (compChoice){
-  case 0:
-    compChoice = "rock"
-      break;
-      case 1:
-          compChoice = "paper";
-          break;
-          case 2:
-              compChoice = "scissors";
-              break;
-}
-
-compare(playerChoice, compChoice);
-
-alert("Next Round");
-//fouth round
-playerChoice = getPlayerChoice();
-compChoice = getComputerChoice(3);
-switch (compChoice){
-  case 0:
-    compChoice = "rock"
-      break;
-      case 1:
-          compChoice = "paper";
-          break;
-          case 2:
-              compChoice = "scissors";
-              break;
-}
-
-compare(playerChoice, compChoice);
-
-alert("Next Round");
-//fifth round
-playerChoice = getPlayerChoice();
-compChoice = getComputerChoice(3);
-switch (compChoice){
-  case 0:
-    compChoice = "rock"
-      break;
-      case 1:
-          compChoice = "paper";
-          break;
-          case 2:
-              compChoice = "scissors";
-              break;
-}
-
-compare(playerChoice, compChoice);
-
-let winner = (playerScore > computerScore) ? `Player Wins: Player's Score = ${playerScore}`: (playerScore < computerScore) ? `Computer wins: Computer's Score = ${computerScore}`:
-`It's a tie! \nPlayer's Score = ${playerScore} \nComputer's Score = ${computerScore}`;
-
-alert(winner)
-
-alert("Inspect Browser Console for Score!")
-
-console.log(`Player's Score =  ${playerScore}`);
-console.log(`Computer's Score = ${computerScore}`);
+round(playerChoice, compChoice)
